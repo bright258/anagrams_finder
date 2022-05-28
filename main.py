80bd54b2ba5010fccf2bdaa1e6551414b1baf04f
bright258 <76488521+bright258@users.noreply.github.com>
2022-05-28 17:26:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+
+    if (sorted(word) == sorted(anagram)):
+        print('True')
+    else:
+        print("False")
+    
+    return True    
+
+find_anagrams('angel', 'glean');
+
